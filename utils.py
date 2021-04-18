54eb265a0e7d20507ded4293ffd98084fa628642
Eduardo <dudarenz@gmail.com>
2021-04-18 16:04:04 -0300

    passing some functions to utils

--- utils.py
+++ utils.py
@@ -1,5 +1,21 @@
-def sign(endpoint,nonce,data):
-  total_params = f"v1/{endpoint}{nonce}{data}"
-  total_params = total_params.encode('utf_8')
-  signature = hmac.new(API_SECRET, total_params, hashlib.sha384).hexdigest()
-  print("signature = {0}".format(signature))
+# Util functions
+def sign(endpoint,nonce,data=''):
+  param_string = f"v1/{endpoint}{nonce}{data}"
+  encoded_param_string = param_string.encode('utf-8')
+  base64_param_string = base64.b64encode(encoded_param_string)
+  signature = hmac.new(API_SECRET.encode(), base64_param_string, hashlib.sha384).hexdigest()
+  return signature
+
+# Generate a headers
+def headers(endpoint,data={}):
+    nonce_now = nonce()
+    return {
+        "Content-Type": "application/json",
+        "BSCNT-NONCE": nonce_now,
+        "BSCNT-APIKEY": API_KEY,
+        "BSCNT-SIGN": sign(endpoint,nonce_now,data),
+    }
+
+
+# Generate a nonce
+nonce = lambda  : str(int(dt.now().timestamp() * 1000))
