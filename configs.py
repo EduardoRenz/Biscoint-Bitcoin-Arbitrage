aa6d19dfed256d2ac9d919831a5838027c957a96
Eduardo <dudarenz@gmail.com>
2021-04-21 19:43:33 -0300

    removed and update unused constants

--- configs.py
+++ configs.py
@@ -6,13 +6,11 @@ load_dotenv()
 #Log config
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="a+",format="%(asctime)-15s %(levelname)-8s %(message)s")
 logging.getLogger().addHandler(logging.StreamHandler()) # Habilita para printar os logs na tela
-#handler.setLevel(logging.DEBUG)
 
 # Constants
 BASE_URL="https://api.biscoint.io/v1"
-AMOUNT = '0.00018'
 BRL_AMOUNT_TRADE = 55 # Amount of BRL to trade  in each transaction
 MIN_PERCENT_REQUIRED = 0.1 # Minimal percentage of spread to do trade
-UPDATE_TICK_RATE = 10 # Update information of ticker each n times
+UPDATE_TICK_RATE = 30 # Update information of ticker each n times
 API_KEY = os.getenv('API_KEY') 
 API_SECRET= os.getenv('API_SECRET') 
