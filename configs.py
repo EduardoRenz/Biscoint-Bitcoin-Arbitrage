119cfbf108db3bb577ef8aa53c6f1d7da92779e6
Eduardo <dudarenz@gmail.com>
2021-10-10 02:47:22 -0300

    add new utils

--- configs.py
+++ configs.py
@@ -3,14 +3,17 @@ import os
 import logging
 load_dotenv()
 
-#Log config
-logging.basicConfig(level=logging.INFO, filename="log.log", filemode="a+",format="%(asctime)-15s %(levelname)-8s %(message)s")
-logging.getLogger().addHandler(logging.StreamHandler()) # Habilita para printar os logs na tela
+# Log config
+logging.basicConfig(level=logging.INFO, filename="log.log",
+                    filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
+# Habilita para printar os logs na tela
+logging.getLogger().addHandler(logging.StreamHandler())
 
 # Constants
-BASE_URL="https://api.biscoint.io/v1"
-BRL_AMOUNT_TRADE = 55 # Amount of BRL to trade  in each transaction
-MIN_PERCENT_REQUIRED = 0.1 # Minimal percentage of spread to do trade
-UPDATE_TICK_RATE = 30 # Update information of ticker each n times
-API_KEY = os.getenv('API_KEY') 
-API_SECRET= os.getenv('API_SECRET') 
+BASE_URL = "https://api.biscoint.io/v1"
+BRL_AMOUNT_TRADE = 55  # Amount of BRL to trade  in each transaction
+MIN_PERCENT_REQUIRED = 0.1  # Minimal percentage of spread to do trade
+UPDATE_TICK_RATE = 30  # Update information of ticker each n times
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+LOGIN_TOKEN = os.getenv('LOGIN_TOKEN')
--- utils.py
+++ utils.py
@@ -3,39 +3,63 @@ import hmac
 import hashlib
 import base64
 import time
+import random
+import string
+from configs import logging
 from configs import API_KEY, API_SECRET, UPDATE_TICK_RATE
 incrementer = 0
 
-def sign(endpoint:str,nonce:int,data:str=''):
-        strToBeSigned = ('v1/%s%d%s' % (endpoint, nonce, data)).encode('utf-8')
-        hashBuffer = base64.b64encode(strToBeSigned)
-        sign_data = hmac.new(
-            API_SECRET.encode(),
-            hashBuffer,
-            hashlib.sha384
-        ).hexdigest()
-        return sign_data
+
+def sign(endpoint: str, nonce: int, data: str = ''):
+    strToBeSigned = ('v1/%s%d%s' % (endpoint, nonce, data)).encode('utf-8')
+    hashBuffer = base64.b64encode(strToBeSigned)
+    sign_data = hmac.new(
+        API_SECRET.encode(),
+        hashBuffer,
+        hashlib.sha384
+    ).hexdigest()
+    return sign_data
 
 
 # Generate a headers
-def headers(endpoint:str,data:str=''):
+def headers(endpoint: str, data: str = ''):
     nonce_now = nonce()
     return {
         "Content-Type": "application/json",
         "BSCNT-NONCE": str(nonce_now),
         "BSCNT-APIKEY": API_KEY,
-        "BSCNT-SIGN": sign(endpoint,nonce_now,data),
+        "BSCNT-SIGN": sign(endpoint, nonce_now, data),
     }
 
 
-nonce = lambda  : int(time.time() * 1000000) # Generate a nonce
-percent = lambda   value1, value2  :  round((float(value2) / float(value1) - 1) * 100,3) # Calculate percent by numbers
-can_buy = lambda brl,min_brl=10 : float(brl) >= float(min_brl) # verify if you have enough money
-btcToTrade = lambda max_brl,quote_amount,base_amount :  (max_brl * base_amount) / quote_amount # calculate min value of btc to trade from brl
+def nonce(): return int(time.time() * 1000000)  # Generate a nonce
 
+
+def percent(value1, value2): return round(
+    (float(value2) / float(value1) - 1) * 100, 3)  # Calculate percent by numbers
+
+
+def can_buy(brl, min_brl=10): return float(brl) >= float(
+    min_brl)  # verify if you have enough money
+
+
+def btcToTrade(max_brl, quote_amount, base_amount): return (
+    max_brl * base_amount) / quote_amount  # calculate min value of btc to trade from brl
 
 
 # Log the current cycle every n times on loop
-def showCycle(cycle_count, percent, at_every:int=20):
+def showCycle(cycle_count, percent, at_every: int = 20):
     if cycle_count % at_every == 0:
         logging.info(f"Cycle {cycle_count} | Last percent : {percent} |")
+
+
+def gen_random(size=17):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(size))
+
+# Generate params of offer
+
+
+def genOfferParams(base='BTC', quote='BRL', op="buy", amount=0.0001, is_quote=False):
+    params = {"base": base, "quote": quote, "op": op,
+              "amount": "0.0001", "isQuote": is_quote, "requestId": gen_random()}
+    return params
